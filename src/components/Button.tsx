0741eb1b54c0ee412b9d264d804c0567c3f31a40
Alex <ajoshua14@icloud.com>
2024-01-05 14:22:58 -0800

    Create a quick button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,30 @@
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import { FC, HTMLAttributes } from 'react'
+
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  text?: string
+  arrow?: boolean
+  variant?: 'primary' | 'secondary'
+}
+
+const Button: FC<ButtonProps> = ({ className, text, arrow, variant, ...props }) => {
+  return (
+    <button className={cn(`${variant === 'secondary' ? "w-20 h-20" : "px-8 py-6"} flex items-center justify-center gap-6 btn`, className)} {...props}>
+      {text &&
+        <p>
+          {text}
+        </p>
+      }
+      {arrow && (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+          <g fill="none" fill-rule="evenodd" stroke="#fff" stroke-width="2">
+            <path d="M15 1l9 9-9 9M0 10h24" />
+          </g>
+        </svg>
+      )}
+    </button>
+  )
+}
+
+export default Button
