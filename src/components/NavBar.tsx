60db80a4e530d1fff0516f326c5f23e2b636460c
Alex <ajoshua14@icloud.com>
2024-01-05 13:58:39 -0800

    Initial commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { FC } from 'react'
+
+interface NavBarProps {
+
+}
+
+const navOptions = [
+  {
+    name: 'Portfolio',
+    path: '/portfolio'
+  },
+  {
+    name: 'About Us',
+    path: '/about'
+  },
+  {
+    name: 'Contact',
+    path: '/contact'
+  },
+]
+
+const NavBar: FC<NavBarProps> = ({ }) => {
+  const pathname = usePathname()
+
+  let pageName = pathname
+
+  if (pathname === '/') {
+    pageName = 'Home'
+  }
+
+  pageName = pageName.replace('/', '').toUpperCase()
+
+
+
+  return (
+    <>
+      <div className="z-50 absolute top-0 left-0 translate-x-1/2 -translate-y-1/2 w-[154px]">
+        <div className="cursor-vertical-text flex items-center gap-12 w-fit origin-left rotate-90">
+          <div className="w-[104px] h-[2px] bg-gray-100" />
+          <p className="text-gray-100 text-lg/[24px] tracking-[18px]">
+            {pageName}
+          </p>
+        </div>
+      </div>
+      <nav className="sticky top-0 left-0 flex gap-14 items-center navBar backdrop-blur-3xl">
+        <Link href="/" className="text-4xl font-extrabold tracking-tighter">
+          Arch
+        </Link>
+        <ul className="flex gap-14">
+          {navOptions.map((option) => (
+            <li key={`nav-${option.name}`}>
+              <Link href={option.path} className="link">
+                {option.name}
+              </Link>
+            </li>
+          ))
+
+
+          }
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar
