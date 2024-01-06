f1aed760ad92e3b338d442f20dcd11e219c023e6
Alex <ajoshua14@icloud.com>
2024-01-05 19:10:35 -0800

    Create quick hook to grab screen width

---
+++ src/lib/hooks/useScreenSize.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+
+
+/**
+ * Used to get the screen size
+ * @returns 
+ */
+export const useScreenSize = () => {
+  const [screenSize, setScreenSize] = useState<'desktop' | 'tablet' | 'mobile'>('desktop')
+  const [width, setWidth] = useState<number>(1440)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setScreenSize('desktop')
+        setWidth(window.innerWidth)
+      } else if (window.innerWidth > 375) {
+        setScreenSize('tablet')
+        setWidth(window.innerWidth)
+      } else {
+        setScreenSize('mobile')
+        setWidth(window.innerWidth)
+      }
+    }
+
+    if (window === undefined) return
+    handleResize()
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  })
+
+  return { screenSize, width }
+}
