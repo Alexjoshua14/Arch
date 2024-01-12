31386869e2c4d965b4068053e94c2d436969b25d
Alex <ajoshua14@icloud.com>
2024-01-12 15:06:31 -0800

    Extract Contact Info component into separate file and add hover effect for 'view on map'

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,7 @@
 import ContactForm from "@/components/ContactForm"
 import Hero from "@/components/Hero"
 import Map from "@/components/Map"
-import Image from "next/image"
-import { FC } from "react"
+import { ContactInfo } from "@/components/ContactInfo"
 
 
 const heroContent = {
@@ -14,55 +13,7 @@ const heroContent = {
 
 }
 
-interface ContactInfoProps {
-  title: string
-  mail: string
-  address: string
-  phone: string
-  mapLink: string
-}
 
-const ContactInfo: FC<ContactInfoProps> = ({ title, mail, address, phone, mapLink }) => {
-  return (
-    <div className="flex-1 w-full flex justify-between flex-col xs:flex-row lg:flex-col gap-8 xs:gap-4 max-w-[600px]">
-      <div className="flex flex-col gap-4 text-gray-700">
-        <h3 className="font-bold">
-          {title}
-        </h3>
-        <div className="flex flex-col">
-          <div className="flex gap-2">
-            <p>Mail:</p>
-            <p>
-              {mail}
-            </p>
-          </div>
-          <div className="flex gap-2">
-            <p>Address:</p>
-            <p>
-              {address}
-            </p>
-          </div>
-          <div className="flex gap-2">
-            <p>Phone:</p>
-            <p>
-              {phone}
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="text-black flex gap-4 font-medium xs:self-end lg:self-auto">
-        <p>
-          {`View on Map`}
-        </p>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-          <g fill="none" fillRule="evenodd" stroke="#000" strokeWidth="2">
-            <path d="M15 1l9 9-9 9M0 10h24" />
-          </g>
-        </svg>
-      </div>
-    </div>
-  )
-}
 
 export default function Contact() {
   return (
---
+++ src/components/ContactInfo.tsx
@@ -0,0 +1,51 @@
+import { FC } from 'react'
+
+interface ContactInfoProps {
+  title: string
+  mail: string
+  address: string
+  phone: string
+  mapLink: string
+}
+
+export const ContactInfo: FC<ContactInfoProps> = ({ title, mail, address, phone, mapLink }) => {
+  return (
+    <div className="flex-1 w-full flex justify-between flex-col xs:flex-row lg:flex-col gap-8 xs:gap-4 max-w-[600px]">
+      <div className="flex flex-col gap-4 text-gray-700">
+        <h3 className="font-bold">
+          {title}
+        </h3>
+        <div className="flex flex-col">
+          <div className="flex gap-2">
+            <p>Mail:</p>
+            <p>
+              {mail}
+            </p>
+          </div>
+          <div className="flex gap-2">
+            <p>Address:</p>
+            <p>
+              {address}
+            </p>
+          </div>
+          <div className="flex gap-2">
+            <p>Phone:</p>
+            <p>
+              {phone}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="text-black flex gap-4 font-medium xs:self-end lg:self-auto cursor-pointer group">
+        <p className="group-hover:underline">
+          {`View on Map`}
+        </p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+          <g fill="none" fillRule="evenodd" stroke="#000" strokeWidth="2">
+            <path d="M15 1l9 9-9 9M0 10h24" />
+          </g>
+        </svg>
+      </div>
+    </div>
+  )
+}
