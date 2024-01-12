527469adf36a382348f47320d15acb028c901b1c
Alex <ajoshua14@icloud.com>
2024-01-12 15:17:32 -0800

    Remove side title on mobile screens

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -38,7 +38,7 @@ const NavBar: FC<NavBarProps> = ({ }) => {
 
   return (
     <>
-      <div className="z-50 absolute top-0 left-0 translate-x-1/2 -translate-y-1/2 whitespace-nowrap nav-side">
+      <div className="z-50 absolute top-0 left-0 translate-x-1/2 -translate-y-1/2 whitespace-nowrap nav-side hidden xs:block">
         <div className="cursor-vertical-text flex items-center gap-12 w-fit origin-left rotate-90">
           <div className="w-[104px] h-[2px] bg-gray-100" />
           <p className="text-gray-100 text-lg/[24px] tracking-[18px]">
