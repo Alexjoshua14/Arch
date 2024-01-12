c909c3d43e6a3bdac2cb3bc7f526d49c1a734446
Alex <ajoshua14@icloud.com>
2024-01-11 16:25:51 -0800

    Responsively update side title width

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -38,7 +38,7 @@ const NavBar: FC<NavBarProps> = ({ }) => {
 
   return (
     <>
-      <div className="z-50 absolute top-0 left-0 translate-x-1/2 -translate-y-1/2 w-[154px] whitespace-nowrap">
+      <div className="z-50 absolute top-0 left-0 translate-x-1/2 -translate-y-1/2 whitespace-nowrap nav-side">
         <div className="cursor-vertical-text flex items-center gap-12 w-fit origin-left rotate-90">
           <div className="w-[104px] h-[2px] bg-gray-100" />
           <p className="text-gray-100 text-lg/[24px] tracking-[18px]">
