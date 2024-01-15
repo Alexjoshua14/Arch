6ff5366f3ec92fc8133677303d765740b9547ccf
Alex <ajoshua14@icloud.com>
2024-01-15 15:43:57 -0800

    Create layout for footer for mobile screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,15 +14,24 @@ interface FooterProps {
  */
 const Footer: FC<FooterProps> = ({ }) => {
   return (
-    <div className="relative footer flex items-center justify-center">
-      <div className="relative h-full w-full max-w-6xl flex">
-        <div className="h-full aspect-square bg-gray-900 flex items-center justify-center">
+    <div className="w-screen h-fit flex justify-center">
+      <div
+        className={`relative footer 
+                      h-full w-full 
+                      pt-20 md:pt-0
+                      flex flex-col 
+                      items-center 
+                      md:flex-row md:items-start 
+                      bg-gray-100 md:bg-transparent
+                      `}
+      >
+        <div className="absolute top-0 -translate-y-1/2 md:translate-y-0 md:relative h-[120px] md:h-full w-[120px] md:w-auto aspect-square bg-gray-900 flex items-center justify-center">
           <span className="text-5xl font-extrabold tracking-tighter text-white">
             {`Arch`}
           </span>
         </div>
-        <div className="h-full w-[73%] bg-gray-100 px-24">
-          <ul className="h-full flex items-center gap-16">
+        <div className="h-full w-2/3 bg-gray-100 px-10 lg:px-24">
+          <ul className="h-full flex flex-col md:flex-row items-center justify-center md:justify-start gap-8 md:gap-16">
             {navOptions.map((option) => (
               <li key={`footer-nav-${option.name}`}>
                 <Link href={option.path} className="link">
@@ -33,7 +42,7 @@ const Footer: FC<FooterProps> = ({ }) => {
           </ul>
         </div>
 
-        <div className="absolute right-0 top-1/2 -translate-y-1/2">
+        <div className="py-8 md:absolute md:right-10 md:top-1/2 md:-translate-y-1/2">
           <Button text="See Our Portfolio" arrow />
         </div>
       </div>
